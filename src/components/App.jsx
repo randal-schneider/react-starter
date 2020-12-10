3cfc5537610f7a4b55cd30217ca413e00029ee3d
Randal Schneider <schneider.randal@gmail.com>
2020-12-09 23:32:03 -0600

    Added search bar and add movie bar but not yet functional

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,40 @@
 import React from 'react';
 import movies from './movies.js';
 import MovieList from './MovieList.jsx'
+import Search from './Search.jsx'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      movies: []
+      movies: [],
+      searched: false
     };
   }
 
-  addMovies(movies) {
-    this.setState({
-      movies: this.movies
-    })
-  }
+  // addMovies(movies) {
+  //   this.setState({
+  //     movies: this.movies
+  //   })
+  // }
 
   render(){
     console.log(movies);
     return (
       <div>
         <div className='addtoMovies'>
-          Add to movies will be here
+          <div className='searchBox'>
+            <input type="text" placeholder="Add to Movie List"></input>
+             <button type="submit"><i className="fa fa-car"></i></button>
+          </div>
         </div>
-        <div className='Search'>
-          Search will be here
+        <div className='search'>
+          <Search />
+          <div className='searchBox'>
+            <input type="text" placeholder="Search For Movies"></input>
+            <button type="submit"><i className="fa fa-search"></i></button>
+          </div>
         </div>
         <div className='movie-list'>
           <MovieList movies={movies} text={'quote'} />
---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+var Search = (props) => {
+  console.log(props);
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Search
