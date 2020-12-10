de5edaae73912d360a600b55b442f21dd076cad6
Randal Schneider <schneider.randal@gmail.com>
2020-12-09 18:49:22 -0600

    Have items rendering, but not yet rendering movies

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import movies from './movies.js';
+import MovieList from './MovieList.js'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: []
+    };
+  }
+
+  addMovies(movies) {
+    this.setState({
+      movies: this.movies
+    })
+  }
+
   render(){
-    return(
-    <div>
-      Starter App component - hello there
+    console.log(movies);
+    return (
+    <div className='movie-list'>
+      <MovieList movies={movies}/>
+      hello from App
     </div>
   )}
 }
 
 export default App;
+
---
+++ src/components/MovieList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+var MovieList = (movies) => {
+console.log(movies.movies);
+  return (
+    <div className="movie-list">
+      Hello from MovieList
+    </div>
+  );
+};
+
+export default MovieList
+
